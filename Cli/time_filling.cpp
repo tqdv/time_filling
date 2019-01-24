027f256b45c91e5eafc3765830acf3f706c31cc4
Tilwa Qendov <tilwa.qendov@gmail.com>
2019-01-24 16:20:29 +0100

    Change the main file a bit

--- Cli/time_filling.cpp
+++ Cli/time_filling.cpp
@@ -11,12 +11,16 @@
 int main (int argc, char **argv) {
 	using std::sqrt;
 	const float a = 5;
-
-	Maybe<Hexagon> outer = Hexagon::make_hexagon (a * (1 + sqrt (3)), M_PI / 6);
+	
+	Maybe<Hexagon> outermost = Hexagon::make_hexagon (sqrt(7) * a * (1 + sqrt(3)), M_PI / 48);
+	Maybe<Hexagon> outer = Hexagon::make_hexagon (a * (1 + sqrt(3)), M_PI / 6);
 	Maybe<Hexagon> inner = Hexagon::make_hexagon (a, 0);
-	if (!outer.has_value() || !inner.has_value()) { return -1; }
 
-	std::vector<Shape*> shapes {&(*outer), &(*inner)};
+	if (!outermost.has_value() || !outer.has_value() || !inner.has_value()) {
+		return -1;
+	}
+
+	std::vector<Shape*> shapes {&(*outermost), &(*outer), &(*inner)};
 
 	Maybe<PointPath> pos = shapes_positions (shapes, {5 * a, 5 * a}, {});
 	if (!pos.has_value()) {return -1; }

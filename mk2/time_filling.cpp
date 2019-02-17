b2cdbee22fbdb6c589f855ba24c29254b9f41162
Tilwa Qendov <tilwa.qendov@gmail.com>
2019-02-17 11:45:22 +0100

    Refactor get_itinerary into another function

--- mk2/time_filling.cpp
+++ mk2/time_filling.cpp
@@ -1,6 +1,4 @@
 #include "time_filling.hpp"
-#include <random>
-// std::random_device et al.
 #include <vector>
 using std::vector;
 #include <list>
@@ -8,6 +6,7 @@ using std::list;
 #include <iterator>
 using std::next;
 #include "hexagon_ops.hpp"
+#include "utils.hpp"
 
 namespace t_fl {
 
@@ -28,7 +27,23 @@ bool Itinerary::is_loop () const {
 
 /* */
 
-// TODO: Refactor those two do ... while loops
+/* Assign the to be connected ends of the end of the list, given $hexs */
+/* Returns the start of the next itinerary */
+HexPoint assign_next_bridge (cr <vector <Hexagon>> hexs, list <Itinerary> &l) {
+	// We assume there is an element after this one
+	auto rbegin = l.rbegin ();
+	int n = l.size ();
+
+	HexPoint f;
+	do {
+		auto [to, from] = link_between (hexs[n-1], hexs[n]);
+		rbegin->ends.to = to;
+		f = from; // Just in case because of scoping
+	} while (rbegin->is_loop ());
+
+	return f;
+}
+
 list<Itinerary> get_itinerary
  (vector<Hexagon> hexs, HexPoint from, HexPoint to) {
 	/* We assume that:
@@ -36,74 +51,32 @@ list<Itinerary> get_itinerary
 	 * + two consecutive $hexs are neighbours
 	 */
 	int n = hexs.size();
-	/*
-	vector <Endpoints> ends(n);
-	ends.front().from = from;
-	ends.back().to = to;
-	*/
 
 using std::cout;
 cout << "Before loop\n";
 
 	list <Itinerary> l;
+	
 
 	HexPoint f = from;  // Used in the loop
 	for (int i = 0; i+1 < n; i++) {
-cout << "In first loop\n";
+cout << "Filling itineraries\n";
 		Itinerary &elt = l.emplace_back (Itinerary (hexs[i]));
 		elt.ends.from = f;
-
-		// A path always exists
-		do {
-cout << "In dowhile loop\n";
-			auto [to, from] = link_between(hexs[i], hexs[i+1]);
-			elt.ends.to = to;
-			f = from;
-		} while (elt.is_loop ());
+		f = assign_next_bridge (hexs, l);
 	}
 
-	// Make sure the last itinerary isn't a loop
 	Itinerary &elt = l.emplace_back (Itinerary (hexs.back()));
+	elt.ends.from = f;
 	elt.ends.to = to;
-	if (n > 1) {
-cout << "Checking last element\n";
-		elt.ends.from = f;  // From the last loop
 
-		do {
-			auto [to, from] = link_between(hexs[n-2], hexs[n-1]);
-			next (l.rbegin ())->ends.to = to;
-			elt.ends.from = from;
-		} while (elt.is_loop ());
-	}
-		
-
-
-
-//	/* Figure out the links (or neighbours) between each hex */
-//	for (int i = 0; i+1 < n; i++) {
-//cout << "In loop\n";
-//		// A path always exists
-//		do {
-//cout << "Trying to get a link\n";
-//			auto [t, f] = link_between(hexs[i], hexs[i+1]);
-//			ends[i].to = t;
-//			ends[i+1].from = f;
-//		} while (!(!ends[i].is_loop() || hexs[i].size == 0));
-//	}
-//	// Make sure the last itinerary isn't a loop
-//	if (n > 1) {
-//		do {
-//cout << "Trying to solve last loop\n";
-//			auto [t, f] = link_between(hexs[n-2], hexs[n-1]);
-//			ends[n-2].to = t;
-//			ends[n-1].from = f;
-//		} while (!(!ends[n-1].is_loop() || hexs[n-1].size == 0));
-//	}
-//
-//	list <Itinerary> l;
-//	for (int i = 0; i < n; i++) {
-//		l.push_back (Itinerary (hexs[i], ends[i]));
-//	}
+	/* Previous comment:
+	// Make sure the last itinerary isn't a loop
+	 * Actually, scrap that, too much of a hassle because if it works, but
+	 * turns the one before it into a loop, you have to throw everything away
+	 * and start again.
+	 * Deterministic algorithms, yay
+	 */
 
 	return l;
 }
--- mk2/time_filling.hpp
+++ mk2/time_filling.hpp
@@ -4,10 +4,12 @@
 #include <list>
 #include "point.hpp"
 #include "hexagon.hpp"
+#include "utils.hpp"
 
 
 namespace t_fl {
 
+/* Endpoints is a pair of HexPoints that define the ends of a path */
 struct Endpoints {
 	HexPoint from;
 	HexPoint to;
@@ -15,6 +17,8 @@ struct Endpoints {
 	bool is_loop () const;
 };
 
+/* An Itinerary is a path through a hexagon, defined by the said hexagon
+ * and endpoints */
 struct Itinerary {
 	Hexagon hex;
 	Endpoints ends;
@@ -22,11 +26,14 @@ struct Itinerary {
 	Itinerary (Hexagon, Endpoints);
 	Itinerary (Hexagon);
 
+	/* Check if the itinerary makes you return to your starting point */
 	bool is_loop() const;
 };
 
 
 
+/* Helper function for the next one */
+HexPoint assign_next_bridge (cr <vector <Hexagon>> hexs, std::list <Itinerary> &l);
 std::list<Itinerary> get_itinerary (Hexagon_v, HexPoint, HexPoint);
 
 }  // namespace t_fl
